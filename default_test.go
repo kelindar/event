cef79e11431d7639b763dbc3607f78f549b12e0e
Roman Atachiants <roman.atachiants@gmail.com>
2022-12-26 14:28:03 +0400

    bench

--- default_test.go
+++ default_test.go
@@ -15,16 +15,16 @@ import (
 
 /*
 cpu: Intel(R) Core(TM) i7-9700K CPU @ 3.60GHz
-BenchmarkEvent/1x1-8         	19471857	        61.37 ns/op	  16295560 ev/s	       0 B/op	       0 allocs/op
-BenchmarkEvent/10x1-8        	  701856	      1772 ns/op	   5643499 ev/s	       0 B/op	       0 allocs/op
-BenchmarkEvent/1x10-8        	 4033690	       389.9 ns/op	  25646847 ev/s	       0 B/op	       0 allocs/op
-BenchmarkEvent/10x10-8       	  107130	     12355 ns/op	   8093423 ev/s	       2 B/op	       0 allocs/op
-BenchmarkEvent/1x100-8       	  378753	      3680 ns/op	  27177107 ev/s	      16 B/op	       0 allocs/op
-BenchmarkEvent/10x100-8      	   28020	     46035 ns/op	  21738149 ev/s	     308 B/op	       0 allocs/op
+BenchmarkEvent/1x1-8         	21140456	        61.46 ns/op	  16270375 ev/s	       0 B/op	       0 allocs/op
+BenchmarkEvent/1x10-8        	 2968579	       404.6 ns/op	  24713378 ev/s	       0 B/op	       0 allocs/op
+BenchmarkEvent/1x100-8       	  333204	      3591 ns/op	  27848968 ev/s	      15 B/op	       0 allocs/op
+BenchmarkEvent/10x1-8        	  685381	      1776 ns/op	   5630000 ev/s	       0 B/op	       0 allocs/op
+BenchmarkEvent/10x10-8       	  115762	     12810 ns/op	   7806533 ev/s	       3 B/op	       0 allocs/op
+BenchmarkEvent/10x100-8      	   28773	     48305 ns/op	  20700046 ev/s	     239 B/op	       0 allocs/op
 */
 func BenchmarkEvent(b *testing.B) {
-	for _, subs := range []int{1, 10, 100} {
-		for _, topics := range []int{1, 10} {
+	for _, topics := range []int{1, 10} {
+		for _, subs := range []int{1, 10, 100} {
 			b.Run(fmt.Sprintf("%dx%d", topics, subs), func(b *testing.B) {
 				var count atomic.Int64
 				for i := 0; i < subs; i++ {
